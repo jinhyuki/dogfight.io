9b27f5212325640afc80439bf50d0599180e4476
Jin Cho <jinhyuki@gmail.com>
2016-06-27 11:11:11 +0900

    change

--- bin/dogfight.sh
+++ bin/dogfight.sh
@@ -5,10 +5,8 @@ elif [ "$1" = "serve" ]; then
     (cd ../; sproutcore server --allow-from-ips='*.*.*.*')
 elif [ "$1" = "build" ]; then
     (cd ../; sproutcore build --buildroot=./build --languages=en)
-    (cd ../; find ./build/ -type f -name '*.js' -o -name '*.css' -o -name '*.html' -exec sed -i '' -e 's/\"\/static\/sproutcore/\"\/df\/build\/static\/sproutcore/g' {} \;)
-    (cd ../; find ./build/ -type f -name '*.js' -o -name '*.css' -o -name '*.html' -exec sed -i '' -e 's/\"\/static\/df/\"\/df\/build\/static\/df/g' {} \;)
-    #(cd ../; find ./build/ -type f -name '*.js' -o -name '*.css' -o -name '*.html' -exec sed -i '' -e 's/\/df\/build\/static\/sproutcore/\/static\/sproutcore/g' {} \;)
-    #(cd ../; find ./build/ -type f -name '*.js' -o -name '*.css' -o -name '*.html' -exec sed -i '' -e 's/\/df\/build\/static\/df/\/static\/df/g' {} \;)
+    (cd ../; find ./build/ -type f -name '*.js' -o -name '*.css' -o -name '*.html' -exec sed -i '' -e 's/\"\/static\/sproutcore/\"\/dogfight.io\/build\/static\/sproutcore/g' {} \;)
+    (cd ../; find ./build/ -type f -name '*.js' -o -name '*.css' -o -name '*.html' -exec sed -i '' -e 's/\"\/static\/df/\"\/dogfight.io\/build\/static\/df/g' {} \;)
 elif [ "$1" = "commit" ]; then
     (cd ../; git add .)
     (cd ../; git commit -m "Commit")
